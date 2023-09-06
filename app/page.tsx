276c871ec8457ba172c36313f64c9e9071fad990
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-06 15:48:41 -0400

    Some text, working on how I want to position them

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
-    <main>
+    <main className='flex flex-col justify-between'>
       <Navbar />
+      <div className='flex mx-16 justify-between'>
+        <div className='flex flex-col items-start text-5xl gap-2'>
+          <h1>Conrad</h1>
+          <h1>Mo</h1>
+        </div>
+        <div className='flex flex-col justify-center'>
+          <h1>Software Developer</h1>
+          <h1>Studying CS @ University of Toronto</h1>
+        </div>
+      </div>
     </main>
   )
 }
