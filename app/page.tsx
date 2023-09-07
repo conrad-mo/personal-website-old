d058dba9220f4aa8efb0d8dea34ec92bd932b07f
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-07 01:39:14 -0400

    Bro I look soooo drunk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Navbar from "../components/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="ctp-latte dark:ctp-mocha relative">
-      <div className="sticky top-0 z-50 max-h-0">
+      <div className="sticky top-0 max-h-0">
         <Navbar />
       </div>
       <div className="flex justify-center items-center h-screen">
@@ -18,20 +19,25 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex h-screen">
-        <div>About</div>
-        <div>
-          Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the University of
-          Toronto. Coding and playing the piano are my two main passions. Music,
-          in particular, holds a special place in my heart, allowing me to
-          express myself in unique ways.
+      <div className="flex h-screen justify-around">
+        <div className="flex flex-col justify-center gap-11">
+          <div className="text-8xl">About Me</div>
+          <div className="max-w-lg text-ctp-subtext1">
+            Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the
+            University of Toronto. Coding and playing the piano are my two main
+            passions. Music, in particular, holds a special place in my heart,
+            allowing me to express myself in unique ways.
+          </div>
+          <div className="max-w-lg text-ctp-subtext1">
+            When I&apos;m not immersed in the world of music and coding, I enjoy
+            dining out with friends and exploring Toronto&apos;s diverse
+            culinary offerings. Food is a wonderful way to connect, but my true
+            love is the piano. If you&apos;re as passionate about music and
+            enjoy good company, let&apos;s connect and share our stories!
+          </div>
         </div>
-        <div>
-          When I&apos;m not immersed in the world of music and coding, I enjoy dining
-          out with friends and exploring Toronto&apos;s diverse culinary offerings.
-          Food is a wonderful way to connect, but my true love is the piano. If
-          you&apos;re as passionate about music and enjoy good company, let&apos;s connect
-          and share our stories!
+        <div className="flex items-center">
+          <Image src="/pfp.jpg" alt="Profile picture" width={400} height={400}/>
         </div>
       </div>
     </main>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,11 @@
 export default function Navbar() {
   return (
-    <main className="flex justify-end items-center text-xl">
+    <main className="flex justify-end items-center text-xl bg-ctp-base">
       <button className="m-4">Home</button>
       <button className="m-4">About</button>
       <button className="m-4">Skills</button>
       <button className="m-4">Projects</button>
+      <div className="w-8"></div>
     </main>
   )
 }
