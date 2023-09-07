ca8240b4a5402996316769363c7476455579ae8c
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-06 23:14:29 -0400

    About page text without positioning or styling

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,11 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="flex h-screen">
+        <div>About</div>
+        <div>Hi! I'm Conrad Mo, a Computer Science undergrad at the University of Toronto. Coding and playing the piano are my two main passions. Music, in particular, holds a special place in my heart, allowing me to express myself in unique ways.</div>
+        <div>When I'm not immersed in the world of music and coding, I enjoy dining out with friends and exploring Toronto's diverse culinary offerings. Food is a wonderful way to connect, but my true love is the piano. If you're as passionate about music and enjoy good company, let's connect and share our stories!</div>
+      </div>
     </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <main className="flex justify-end items-center">
+    <main className="flex justify-end items-center text-xl">
       <button className="m-4">Home</button>
       <button className="m-4">About</button>
       <button className="m-4">Skills</button>
