cc317f061abb60fcfcf33bf3ac05e045618146d8
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-06 22:02:13 -0400

    Most things well positioned, need better styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col ctp-latte dark:ctp-mocha">
+    <main className="ctp-latte dark:ctp-mocha relative">
+      <div className="absolute top-0 right-0">
       <Navbar />
-      <div className="">
-        <div className="flex mx-16 justify-between">
-          <div className="flex flex-col items-start text-5xl gap-2">
-            <div>Conrad</div>
-            <div>Mo</div>
-          </div>
-          <div className="flex flex-col justify-center">
+      </div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col items-start text-8xl gap-2 m-4">
+          <div>Conrad</div>
+          <div>Mo</div>
+          <div className="flex flex-col text-5xl my-8 gap-6">
             <div className="text-ctp-rosewater">Software Developer</div>
-            <div>Studying CS @ University of Toronto</div>
+            <div className="text-ctp-lavender">Studying CS @ University of Toronto</div>
           </div>
         </div>
       </div>
