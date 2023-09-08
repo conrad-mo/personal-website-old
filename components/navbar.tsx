6e7169a081f0e9ea6d2636e1a3516079cb10f6ec
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-08 01:51:37 -0400

    Basically done website

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,44 @@
-import Link from "next/link";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 export default function Navbar() {
   return (
     <main className="flex justify-end items-center text-xl bg-ctp-base">
-      <Link href="#home">
-        <button className="m-4">Home</button>
-      </Link>
-      <Link href="#aboutme">
-        <button className="m-4">About</button>
-      </Link>
-      <Link href="#skills">
-        <button className="m-4">Skills</button>
-      </Link>
-      <Link href="#projects">
-        <button className="m-4">Projects</button>
-      </Link>
+      <ScrollLink
+        to="home"
+        smooth={true}
+        duration={500}
+        className="m-4"
+        id="homebutton"
+      >
+        <button>Home</button>
+      </ScrollLink>
+      <ScrollLink
+        to="aboutme"
+        smooth={true}
+        duration={500}
+        className="m-4"
+        id="aboutbutton"
+      >
+        <button>About</button>
+      </ScrollLink>
+      <ScrollLink
+        to="skills"
+        smooth={true}
+        duration={500}
+        className="m-4"
+        id="skillsbutton"
+      >
+        <button>Skills</button>
+      </ScrollLink>
+      <ScrollLink
+        to="projects"
+        smooth={true}
+        duration={500}
+        className="m-4"
+        id="projectsbutton"
+      >
+        <button>Projects</button>
+      </ScrollLink>
       <div className="w-8"></div>
     </main>
   );
