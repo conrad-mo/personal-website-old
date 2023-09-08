8d4e12a38919e29d5a9e30aefb32d77a26a6d9d4
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-08 02:14:06 -0400

    Add button border + hover effects

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,41 +3,37 @@ import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 export default function Navbar() {
   return (
     <main className="flex justify-end items-center text-xl bg-ctp-base">
-      <ScrollLink
-        to="home"
-        smooth={true}
-        duration={500}
-        className="m-4"
-        id="homebutton"
-      >
-        <button>Home</button>
+      <ScrollLink to="home" smooth={true} duration={500} className="m-2" id="homebutton">
+        <button className="rounded-lg border-2 border-transparent px-2 py-1 transition-colors hover:border-ctp-text hover:bg-ctp-surface2 active:bg-ctp-surface2/50">
+          Home
+        </button>
       </ScrollLink>
       <ScrollLink
         to="aboutme"
         smooth={true}
         duration={500}
-        className="m-4"
+        className="m-2"
         id="aboutbutton"
       >
-        <button>About</button>
+        <button className="rounded-lg border-2 border-transparent px-2 py-1 transition-colors hover:border-ctp-text hover:bg-ctp-surface2 active:bg-ctp-surface2/50">About</button>
       </ScrollLink>
       <ScrollLink
         to="skills"
         smooth={true}
         duration={500}
-        className="m-4"
+        className="m-2"
         id="skillsbutton"
       >
-        <button>Skills</button>
+        <button className="rounded-lg border-2 border-transparent px-2 py-1 transition-colors hover:border-ctp-text hover:bg-ctp-surface2 active:bg-ctp-surface2/50">Skills</button>
       </ScrollLink>
       <ScrollLink
         to="projects"
         smooth={true}
         duration={500}
-        className="m-4"
+        className="m-2"
         id="projectsbutton"
       >
-        <button>Projects</button>
+        <button className="rounded-lg border-2 border-transparent px-2 py-1 transition-colors hover:border-ctp-text hover:bg-ctp-surface2 active:bg-ctp-surface2/50">Projects</button>
       </ScrollLink>
       <div className="w-8"></div>
     </main>
