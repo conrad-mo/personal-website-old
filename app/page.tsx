0ee32f30916270b3e963abd9966f3a21bc82a6de
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-08 00:08:34 -0400

    Animate everything + speed it up

--- app/page.tsx
+++ app/page.tsx
@@ -35,30 +35,47 @@ export default function Home() {
         className="flex min-h-screen justify-center items-center gap-16 lg:gap-[170px] mx-16 flex-col-reverse lg:flex-row py-16 lg:p-0"
         id="aboutme"
       >
-          <motion.div
-            initial="hide"
-            whileInView="show"
-            exit="hidden"
-            variants={containerVariants}
-            className="flex flex-col justify-center items-center lg:items-start gap-6 lg:gap-11"
-          >
-            <motion.div className="text-5xl lg:text-6xl" variants={fadeIn}>About Me</motion.div>
-            <motion.div className="lg:max-w-md text-ctp-subtext1" variants={fadeIn}>
-              Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the
-              University of Toronto. Coding and playing the piano are my two
-              main passions. Music, in particular, holds a special place in my
-              heart, allowing me to express myself in unique ways.
-            </motion.div>
-            <motion.div className="lg:max-w-md text-ctp-subtext1" variants={fadeIn}>
-              When I&apos;m not immersed in the world of music and coding, I
-              enjoy dining out with friends and exploring Toronto&apos;s diverse
-              culinary offerings. Food is a wonderful way to connect, but my
-              true love is the piano. If you&apos;re as passionate about music
-              and enjoy good company, let&apos;s connect and share our stories!
-            </motion.div>
+        <motion.div
+          initial="hide"
+          whileInView="show"
+          exit="hidden"
+          variants={containerVariants}
+          className="flex flex-col justify-center items-center lg:items-start gap-6 lg:gap-11"
+        >
+          <motion.div className="text-5xl lg:text-6xl" variants={fadeIn}>
+            About Me
           </motion.div>
-        <div className="flex items-center">
-          <div className="flex items-center shrink basis-auto rounded-full overflow-hidden w-[200px] lg:w-[400px] h-[200px] lg:h-[400px]">
+          <motion.div
+            className="lg:max-w-md text-ctp-subtext1"
+            variants={fadeIn}
+          >
+            Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the
+            University of Toronto. Coding and playing the piano are my two main
+            passions. Music, in particular, holds a special place in my heart,
+            allowing me to express myself in unique ways.
+          </motion.div>
+          <motion.div
+            className="lg:max-w-md text-ctp-subtext1"
+            variants={fadeIn}
+          >
+            When I&apos;m not immersed in the world of music and coding, I enjoy
+            dining out with friends and exploring Toronto&apos;s diverse
+            culinary offerings. Food is a wonderful way to connect, but my true
+            love is the piano. If you&apos;re as passionate about music and
+            enjoy good company, let&apos;s connect and share our stories!
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial="hide"
+          whileInView="show"
+          exit="hidden"
+          variants={containerVariants}
+          className="flex items-center"
+        >
+          <motion.div
+            className="flex items-center shrink basis-auto rounded-full overflow-hidden w-[200px] lg:w-[400px] h-[200px] lg:h-[400px]"
+            variants={fadeIn}
+          >
             <Image
               src="/pfp.jpg"
               alt="Profile picture"
@@ -66,65 +83,83 @@ export default function Home() {
               width={400}
               height={400}
             />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
       <div
         className="flex min-h-screen justify-center items-center my-8 py-16 lg:p-0"
         id="skills"
       >
-        <div className="flex flex-col items-center lg:items-start gap-4">
-          <div className="text-5xl lg:text-6xl">Skills</div>
+        <motion.div
+          className="flex flex-col items-center lg:items-start gap-4"
+          initial="hide"
+          whileInView="show"
+          exit="hidden"
+          variants={containerVariants}
+        >
+          <motion.div variants={fadeIn} className="text-5xl lg:text-6xl">
+            Skills
+          </motion.div>
           <div className="flex flex-col lg:flex-row gap-8 lg:gap-48 justify-center lg:justify-between lg:flex-wrap">
             <div className="flex flex-col text-3xl gap-2 items-center justify-center lg:items-start">
-              <div className="text-xl text-ctp-mauve">Programming</div>
+              <motion.div variants={fadeIn} className="text-xl text-ctp-mauve">
+                Programming
+              </motion.div>
               <div className="text-ctp-subtext0 gap-2 flex flex-col items-center lg:items-start">
-                <div>Python</div>
-                <div>C</div>
-                <div>Java</div>
-                <div>Dart</div>
-                <div>HTML/CSS</div>
-                <div>Javascript/Typescript</div>
-                <div>Bash</div>
-                <div>Rust</div>
-                <div>SQL</div>
+                <motion.div variants={fadeIn}>Python</motion.div>
+                <motion.div variants={fadeIn}>C</motion.div>
+                <motion.div variants={fadeIn}>Java</motion.div>
+                <motion.div variants={fadeIn}>Dart</motion.div>
+                <motion.div variants={fadeIn}>HTML/CSS</motion.div>
+                <motion.div variants={fadeIn}>Javascript/Typescript</motion.div>
+                <motion.div variants={fadeIn}>Bash</motion.div>
+                <motion.div variants={fadeIn}>Rust</motion.div>
+                <motion.div variants={fadeIn}>SQL</motion.div>
               </div>
             </div>
             <div className="flex flex-col text-3xl gap-2 items-center lg:items-start">
-              <div className="text-xl text-ctp-yellow">
+              <motion.div variants={fadeIn} className="text-xl text-ctp-yellow">
                 Frameworks/Libraries
-              </div>
+              </motion.div>
               <div className="text-ctp-subtext0 gap-2 flex flex-col items-center lg:items-start">
-                <div>Flutter</div>
-                <div>React</div>
-                <div>Node JS</div>
-                <div>Riverpod</div>
-                <div>Tailwind CSS</div>
-                <div>CI/CD</div>
+                <motion.div variants={fadeIn}>Flutter</motion.div>
+                <motion.div variants={fadeIn}>React</motion.div>
+                <motion.div variants={fadeIn}>Node JS</motion.div>
+                <motion.div variants={fadeIn}>Riverpod</motion.div>
+                <motion.div variants={fadeIn}>Tailwind CSS</motion.div>
+                <motion.div variants={fadeIn}>CI/CD</motion.div>
               </div>
             </div>
             <div className="flex flex-col text-3xl gap-2 items-center lg:items-start">
-              <div className="text-xl text-ctp-green">Tools</div>
+              <motion.div variants={fadeIn} className="text-xl text-ctp-green">
+                Tools
+              </motion.div>
               <div className="text-ctp-subtext0 gap-2 flex flex-col items-center lg:items-start">
-                <div>Git</div>
-                <div>Github</div>
-                <div>Linux</div>
-                <div>Docker</div>
-                <div>Firebase</div>
-                <div>Postman</div>
-                <div>AWS</div>
-                <div>GCP</div>
-                <div>Azure</div>
+                <motion.div variants={fadeIn}>Git</motion.div>
+                <motion.div variants={fadeIn}>Github</motion.div>
+                <motion.div variants={fadeIn}>Linux</motion.div>
+                <motion.div variants={fadeIn}>Docker</motion.div>
+                <motion.div variants={fadeIn}>Firebase</motion.div>
+                <motion.div variants={fadeIn}>Postman</motion.div>
+                <motion.div variants={fadeIn}>AWS</motion.div>
+                <motion.div variants={fadeIn}>GCP</motion.div>
+                <motion.div variants={fadeIn}>Azure</motion.div>
               </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className="flex justify-center items-center p-16" id="projects">
-        <div className="flex flex-col items-center lg:items-start justify-start gap-8 max-w-5xl">
-          <div className="text-5xl lg:text-6xl">Projects</div>
+        <motion.div
+          className="flex flex-col items-center lg:items-start justify-start gap-8 max-w-5xl"
+          initial="hide"
+          whileInView="show"
+          exit="hidden"
+          variants={containerVariants}
+        >
+          <motion.div variants={fadeIn} className="text-5xl lg:text-6xl">Projects</motion.div>
           <div className="flex flex-wrap gap-8 w-full justify-center lg:justify-start">
-            <div className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
+            <motion.div variants={fadeIn} className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
               <Image
                 src="/utscgeoguessr.jpg"
                 alt="UTSC Geoguessr Banner"
@@ -138,8 +173,8 @@ export default function Home() {
                   students learn the campus. Submission for HTV 7
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
+            </motion.div>
+            <motion.div variants={fadeIn} className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
               <Image
                 src="/retina.png"
                 alt="Retina Banner"
@@ -154,8 +189,8 @@ export default function Home() {
                   Submission for UoftHacksX
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
+            </motion.div>
+            <motion.div variants={fadeIn} className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
               <Image
                 src="/viridian.png"
                 alt="Viridian Banner"
@@ -169,8 +204,8 @@ export default function Home() {
                   each other
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
+            </motion.div>
+            <motion.div variants={fadeIn} className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
               <Image
                 src="/speechsummarizer.png"
                 alt="Speech Summarizer Banner"
@@ -184,8 +219,8 @@ export default function Home() {
                   summaries. Winner of Best Mobile Hack at Ignition Hacks 2023
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
+            </motion.div>
+            <motion.div variants={fadeIn} className="flex flex-col w-80 gap-4 border-ctp-mantle border-solid border-4">
               <Image
                 src="/website.png"
                 alt="Website Banner"
@@ -196,9 +231,9 @@ export default function Home() {
                 <div>Website</div>
                 <div>A website to display myself, skills and projects</div>
               </div>
-            </div>
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </main>
   );
@@ -211,7 +246,7 @@ const fadeIn: Variants = {
   show: {
     opacity: 1,
     transition: {
-      duration: 0.5,
+      duration: 0.25,
     },
   },
   exit: { opacity: 0, transition: { duration: 1 } },
@@ -220,8 +255,8 @@ const fadeIn: Variants = {
 const containerVariants = {
   show: {
     transition: {
-      staggerChildren: 0.5, // Delay children by 1 second
-      delayChildren: 0.5, // Start delaying children after 1 second
+      staggerChildren: 0.25, // Delay children by 1 second
+      delayChildren: 0.25, // Start delaying children after 1 second
     },
   },
 };
